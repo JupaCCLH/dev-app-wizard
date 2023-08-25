e0bba87b20cf31f9db0d841de8e7f17804ecad12
Juan Pablo Olguin <jolguin@losheroes.cl>
2023-08-25 14:41:48 -0400

    initial commit

--- components/WizardServer.vue
+++ components/WizardServer.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <h1>Wizard proyecto Github</h1>
-    <Step1 v-if="currentIndex == 0" />
-    <Step2 v-if="currentIndex == 1" />
-    <Step3 v-if="currentIndex == 2" />
+    <Step1Server v-if="currentIndex == 0" />
+    <Step2Server v-if="currentIndex == 1" />
+    <Step3Server v-if="currentIndex == 2" />
 
     <button v-if="currentIndex > 0" @click="useAppStore().step = currentIndex - 1">
       Atras
